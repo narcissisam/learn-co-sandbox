f9ea10a2212d8950164ab852386e0880d6703bd4
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-31 14:06:47 +0000

    automatically backed up by learn

---
+++ test.js
@@ -0,0 +1,11 @@
+var myScope = outerFunction();
+
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
